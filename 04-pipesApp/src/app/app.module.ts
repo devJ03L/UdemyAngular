c30961fa6beadc32a97798f894f3a3486a2f92f1
devJ03L <joel.dev.net@gmail.com>
2023-01-07 22:18:03 -0600

    Clase 151

---
+++ 04-pipesApp/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app-router.module';
+import { VentasModule } from './ventas/ventas.module';
+
+//Cambiar el locale de la app
+import localEsMX from '@angular/common/locales/es-MX';
+import localFr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localEsMX)
+registerLocaleData(localFr)
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    SharedModule,
+    AppRouterModule,
+    VentasModule
+  ],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-MX'
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
