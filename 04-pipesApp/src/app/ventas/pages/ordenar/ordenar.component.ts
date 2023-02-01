8c30784eba11891b6ada276779f8b5de232186f4
devJ03L <joel.dev.net@gmail.com>
2023-01-31 18:05:35 -0600

    Fin seccion 11

--- 04-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
+++ 04-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -7,5 +8,41 @@ import { Component } from '@angular/core';
   ]
 })
 export class OrdenarComponent {
+  enMayusculas: boolean = true
+  ordenarPor: string = 'sin valor'
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna verde',
+      vuela: true,
+      color: Color.verde
+    }
+  ]
 
+  toggleMayusculas(){
+    this.enMayusculas = !this.enMayusculas
+  }
+
+  cambiarOrden(valor: string){
+    this.ordenarPor = valor
+  }
 }
